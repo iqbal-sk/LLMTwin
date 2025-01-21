114baf0eadfaf353d3a63e72978f5a2d90a921c1
Iqbal <mahammad@buffalo.edu>
2025-01-20 20:27:50 -0500

    feat: Implement crawlers and dispatcher for platform-specific data extraction
    - Added crawlers to extract information from:
      - GitHub repositories
      - LinkedIn profiles
      - Medium posts
      - Custom Article posts
    - Developed a dispatcher class to route URLs to the appropriate crawler based on the platform.

--- llm_engineering/settings.py
+++ llm_engineering/settings.py
@@ -6,6 +6,10 @@ class Settings(BaseSettings):
     DATABASE_HOST: str = "mongodb://llm_engineering:llm_engineering@127.0.0.1:27017"
     DATABASE_NAME: str = "twin"
 
+    # LinkedIn Credentials
+    LINKEDIN_USERNAME: str | None = None
+    LINKEDIN_PASSWORD: str | None = None
+
 
 
 settings = Settings()
